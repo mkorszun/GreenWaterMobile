df590297a8f7c4aeccf4f14a33b007e64c58d5ed
Mateusz Korszun <mkorszun@gmail.com>
2015-04-12 13:03:28 +0200

    Cleanup

--- app/src/main/java/com/greenwatermobile/fragments/NavigationFragment.java
+++ app/src/main/java/com/greenwatermobile/fragments/NavigationFragment.java
@@ -241,12 +241,6 @@ public class NavigationFragment extends Fragment {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
